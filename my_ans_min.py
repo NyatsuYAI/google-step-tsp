7071ffbc6c40137ef03b22e18e3278979203786a
NyatsuYAI <92320824+NyatsuYAI@users.noreply.github.com>
2022-06-08 22:49:28 +0900

    add random start

--- my_ans_min.py
+++ my_ans_min.py
@@ -6,9 +6,11 @@ import sys
 import math
 from unittest.util import sorted_list_difference
 import copy
+import random
 
 from common import print_tour, read_input
 
+from common import format_tour
 
 
 def distance(city1, city2):
@@ -17,7 +19,7 @@ def distance(city1, city2):
 
 
    
-def opt_2(tour, dist):
+def opt_2(tour, dist,tour_distance):
     N = len(tour)
 
     while True:
@@ -31,10 +33,11 @@ def opt_2(tour, dist):
                 if l1 + l2 > l3 + l4:
                     new_tour = tour[i+1 : j+1]
                     tour[i+1 : j+1] = new_tour[::-1]
+                    tour_distance-=(l1+l2-l3-l4)
                     count += 1
         if count == 0:
             break
-    return tour
+    return tour, tour_distance
 
 def solve(cities):
     N = len(cities)
@@ -48,27 +51,42 @@ def solve(cities):
                 dist_data [distance(cities[i], cities[j])] = []
             
 
-    current_city = 0
-    unvisited_cities = set(range(1,N))
-    tour = [current_city]
-    tour_distance=0
-    while unvisited_cities:
-        next_city = min(unvisited_cities,
-                            key=lambda city: dist[current_city][city])
-        unvisited_cities.remove(next_city)
-        tour.append(next_city)
-        tour_distance+=distance(cities [current_city],cities[next_city])
-        current_city = next_city
-    opt_2(tour,dist)
+    
 
-    return tour
+    random_check_city=set()
+    for k in range(int(N/5)):
+        if len(random_check_city)<50:
+            random_check_city.add(random.randint(0,N-1))
+        else:
+            break
+    print(random_check_city)
+    pre_set_cities=[num for num in range(0,N)]
+    tour_index={}
+    for current_city in random_check_city:
+        preset_cities_copy=copy.copy(pre_set_cities)
+        preset_cities_copy.remove(current_city)
+        unvisited_cities = set(preset_cities_copy)
+        tour = [current_city]
+        tour_distance=0
+        while unvisited_cities:
+            next_city = min(unvisited_cities,
+                                key=lambda city: dist[current_city][city])
+            unvisited_cities.remove(next_city)
+            tour.append(next_city)
+            tour_distance+=dist[current_city][next_city]
+            current_city = next_city
+        opt_2(tour,dist,tour_distance)
+        tour_index[tour_distance]=tour
+        print(tour_distance)
+    sorted_tour_index=sorted(tour_index.items(), key=lambda x:x[0])
+
+    return sorted_tour_index[0][1]
 
 
 
 if __name__ == '__main__':
     # assert len(sys.argv) > 1
-    cities=read_input("./input_0.csv")
+    cities=read_input("./input_6.csv")
 
     tour= solve(cities)
     print_tour(tour)
-
