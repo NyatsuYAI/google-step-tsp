d505ced6e2a1ff3207f69722cfad05f9d92933cc
NyatsuYAI <92320824+NyatsuYAI@users.noreply.github.com>
2022-06-08 20:40:00 +0900

    solve 2-opt

--- my_ans_min.py
+++ my_ans_min.py
@@ -15,31 +15,25 @@ def distance(city1, city2):
     return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
 
 
-def opt_2(tour,cities):
-    N=len(tour)
+
+   
+def opt_2(tour, dist):
+    N = len(tour)
+
     while True:
-        count=0
-        for i1 in range(N - 2):
-            i2 = i1 + 1
-            for j1 in range(i1 + 2, N):
-                if j1 == N - 1:
-                    j2 = 0
-                else:
-                    j2 = j1 + 1
-                if i1 != 0 or j2 != 0:
-                    l1 = distance(cities[i1],cities[i2])
-                    l2 = distance(cities[j1],cities[j2])
-                    l3 = distance(cities[i1],cities[j1])
-                    l4 = distance(cities[i2],cities[j2])
-                    print(i1,i2,j1,j2)
-                    print(l1+l2,l3+l4)
-                    if l1 + l2 > l3 + l4:
-                        # つなぎかえる
-                        print(tour,count)
-                        new_path = tour[i1:j1+1]
-                        tour[i1:j1+1] = new_path[::-1]
-                        count+=1
-        if count == 0: break
+        count = 0
+        for i in range(N-2):
+            for j in range(i+2, N):
+                l1 = dist[tour[i]][tour[i + 1]]
+                l2 = dist[tour[j]][tour[(j + 1) % N]]
+                l3 = dist[tour[i]][tour[j]]
+                l4 = dist[tour[i + 1]][tour[(j + 1) % N]]
+                if l1 + l2 > l3 + l4:
+                    new_tour = tour[i+1 : j+1]
+                    tour[i+1 : j+1] = new_tour[::-1]
+                    count += 1
+        if count == 0:
+            break
     return tour
 
 def solve(cities):
@@ -65,7 +59,7 @@ def solve(cities):
         tour.append(next_city)
         tour_distance+=distance(cities [current_city],cities[next_city])
         current_city = next_city
-    opt_2(tour,cities)
+    opt_2(tour,dist)
 
     return tour
 
@@ -76,6 +70,5 @@ if __name__ == '__main__':
     cities=read_input("./input_0.csv")
 
     tour= solve(cities)
-    print(tour)
     print_tour(tour)
 
