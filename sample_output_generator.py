ce4cfbfaae4926ac7e6820e431a2c9848ba24639
NyatsuYAI <92320824+NyatsuYAI@users.noreply.github.com>
2022-06-10 15:21:42 +0900

    Add files via upload

--- sample_output_generator.py
+++ sample_output_generator.py
@@ -4,17 +4,21 @@ from common import format_tour, read_input
 
 import solver_greedy
 import solver_random
+import my_ans
+import my_ans_min
+import my_ans_random
 
-CHALLENGES = 7
+CHALLENGES = 6
 
 
 def generate_sample_output():
     for i in range(CHALLENGES):
         cities = read_input(f'input_{i}.csv')
-        for solver, name in ((solver_random, 'random'), (solver_greedy, 'greedy')):
+        for solver, name in ((solver_greedy, 'random'), (solver_random, 'greedy'),(my_ans_min, 'sa')):
             tour = solver.solve(cities)
             with open(f'sample/{name}_{i}.csv', 'w') as f:
                 f.write(format_tour(tour) + '\n')
+        
 
 
 if __name__ == '__main__':
